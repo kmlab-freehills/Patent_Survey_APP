ccbc81319bbe9cca702f53e1e2738e27058f9648
Takahiro Hirano <t24067tt@gmail.com>
2025-12-25 20:18:54 +0900

    環境構築

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Patent_Survey_APP
 
 Hiranoブランチで開発を進めていきます。（2025/12/25記載）
+
+## バックエンドのセットアップ (bash)
+
+### 前提条件
+* bash / git bashが使える
+* uvがインストール済み
+
+### 環境構築コマンド
+```bash
+cd backend
+uv sync
+uv run uvicorn main:app --reload
+```
+
+## フロントエンドのセットアップ（bash）
+
+```bash
+cd frontend
+npm install
+npm run dev
+```
---
+++ frontend/app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans_JP } from "next/font/google";
+import "./globals.css";
+
+// フォント指定
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const notoSansJP = Noto_Sans_JP({
+  variable: "--font-noto-sans-jp",
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+  display: "swap",
+});
+
+// メタデータ設定
+export const metadata: Metadata = {
+  title: "Patent-Survey-App",
+  description: "フリーヒルズラボ",
+};
+
+// アプリ本体
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${notoSansJP.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
